8f6642e46d702c30ef6efc8560e333c83004cd03
BanurekhaV <rekhasairam@hotmail.com>
2023-02-21 14:03:41 +0530

    Day2CSharp part1

---
+++ CSharp/Day2prj/Day2prj/ControlClass.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2prj
+{
+    class ControlClass
+    {
+        //if else
+        public void checkGrade()
+        {
+            char grade;
+            Console.WriteLine("Enter your Grade (O/A/B/C/D :");
+            grade = Convert.ToChar(Console.ReadLine());
+            if (grade == 'O') 
+                Console.WriteLine("outstanding");
+            else if(grade=='A')
+                Console.WriteLine("Excellent");
+            else if (grade == 'B')
+                Console.WriteLine("Very Good");
+            else if (grade == 'C')
+                Console.WriteLine("Good");
+            else if (grade == 'D')
+                Console.WriteLine("Poor");
+            else
+                Console.WriteLine("Invalid Grade");
+        }
+
+        //switch case grade check
+        public void checkGrade_with_Switch()
+        {
+            char grade;
+            Console.WriteLine("Enter your Grade (O/A/B/C/D :");
+            grade = Convert.ToChar(Console.ReadLine());
+            switch(grade)
+            {
+                case 'O': Console.WriteLine("Outstanding"); break;
+                case 'A': Console.WriteLine("Excellent"); break;
+                case 'B': Console.WriteLine("VeryGood"); break;
+                case 'C': Console.WriteLine("Good"); break;
+                case 'D': Console.WriteLine("Poor"); break;
+                default: Console.WriteLine("Invalid Grade"); break;
+            }
+        }
+    }
+
+    class Loops
+    {
+        public static void ForLoop()
+        {
+            int total = 0;
+            for(int i=0; i<=5; i++)
+            {
+                if (i == 3)
+                    // break;
+                    continue;
+                Console.WriteLine("Looping the Values {0}",i);
+                total += i;
+            }
+            Console.WriteLine("The sum of all the loop values is :{0}", total);
+        }
+
+        public void WhileLoop()
+        {
+            int i = 5;   //initialization of a counter value
+            while(i<5)  // condition for iteration
+            {
+                Console.WriteLine("The value of i is :{0}", i);
+                i++;     //change of the initial value
+            }
+            Console.WriteLine("sorry..");
+        }
+
+        public void DoWhileloop()
+        {
+            int i = 4;
+            do
+            {
+                Console.WriteLine("The value of i is :{0}",i);
+                i++;
+            } while (i < 5);
+        }
+
+        public void gotoExample()
+        {
+        label1:
+            Console.WriteLine("Enter a number between 1 and 9");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num>=10)
+            {
+                //transfer the control back to label1
+                goto label1;
+            }
+            Console.WriteLine(num  + " "+ "is less than 10");
+        }
+    }
+}
---
+++ CSharp/Day2prj/Day2prj/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2prj
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            // program.SingleDimension();
+            //program.twoDimension();
+            // program.jaggedArray();
+            ControlClass cc = new ControlClass();
+            // cc.checkGrade();
+            // cc.checkGrade_with_Switch();
+          //  Loops.ForLoop();
+            Loops loops = new Loops();
+            //loops.WhileLoop();
+            // Console.WriteLine("------------");
+            //loops.DoWhileloop();
+            loops.gotoExample();
+            Console.Read();
+        }
+
+        public void SingleDimension()
+        {
+            int[] arr1 = new int[5];
+            Console.WriteLine("Enter Array Elements");
+            for(int i=0; i<arr1.Length;i++)
+            {
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(arr1);
+            Console.WriteLine("------Displaying Data------");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.WriteLine(arr1[i]);
+            }
+            Console.WriteLine(arr1.Rank);
+        }
+
+        public void twoDimension()
+        {
+            int[,] arr2 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            Console.WriteLine(arr2[0,1]); //return 2
+            Console.WriteLine(arr2[1,0]); //returns 4
+
+            //1st loop to track the row
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("Elements at Row " + i + ":");
+                //inner loop to track columns
+                for(int j=0; j < 3;j++)
+                {
+                    Console.Write(arr2[i,j] +" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void jaggedArray()
+        {
+            //decl jagged array of 2 rows
+            int[][] jarray = new int[2][];
+            //1. let us set the size of the columns for each jagged row
+            jarray[0] = new int[3];
+            jarray[1] = new int[2];
+
+            //initialize the jarray
+            jarray[0][0] = 2;
+            jarray[0][1] = 4;
+            jarray[0][2] = 6;
+
+            jarray[1][0] = 3;
+            jarray[1][1] = 5;
+
+            //2 nd way to set the size and initialize data
+            int[][] jarray1 =
+            {
+                new int[]{5,10,15,20},
+                new int[]{25,30},
+                new int[]{35,40,45}
+            };
+
+            //we need 2 lops to iterate the jagged array
+            for(int i=0; i < jarray1.Length;i++)
+            {
+                Console.Write("Elements at Row No : "+ i + " ");
+                for(int j=0; j < jarray1[i].Length;j++)
+                {
+                    Console.Write(jarray1[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
