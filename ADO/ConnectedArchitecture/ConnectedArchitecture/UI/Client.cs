fbbc95a22dee644e760d036e6e829b2f024e323d
BanurekhaV <rekhasairam@hotmail.com>
2023-03-01 18:11:51 +0530

    ADO 1

---
+++ ADO/ConnectedArchitecture/ConnectedArchitecture/UI/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConnectedArchitecture.BAL;
+
+namespace ConnectedArchitecture.UI
+{
+    class Client
+    {
+        static void Main()
+        {         
+            Shippers s = new Shippers();               
+            Console.WriteLine("--------Edit Shipper---------");
+            int rows= s.EditShippers();
+            Console.WriteLine("Rows updated are {0}",rows); 
+            Console.WriteLine();
+           // Console.WriteLine("--------Insert Shipper---------");
+            //int row = s.InsertShippers();
+            Console.WriteLine();
+            SqlDataReader cdr = s.readShippers();
+            while (cdr.Read())
+            {
+                Console.WriteLine(cdr[0]);
+                Console.WriteLine(cdr[1]);
+                Console.WriteLine(cdr[2]);
+            }
+            Console.Read();
+        }
+    }
+}
